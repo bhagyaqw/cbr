89d0abfb166a1068eac7baf9cd9b620a57e9247c
bhagyaqw <92821708+bhagyaqw@users.noreply.github.com>
2022-10-31 23:25:41 +0530

    Add files via upload

---
+++ detail-audio.js
@@ -0,0 +1,20 @@
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
+define(['domReady', 'iscroll'], function(domReady, myScroll) {
+  console.log('Loading detail audio.');
+
+});
---
+++ footer.js
@@ -0,0 +1,29 @@
+define(['jquery'], function($) {
+  console.log('Loading footer.');
+
+  var $footerLink = $('#footer a');
+  switch (location.pathname) {
+
+    case '/':
+      $('#
+      break;
+    case '/search':
+      $('#search-footer').addClass('active');
+      break;
+    case '/settings':
+      $('#settings').addClass('active');
+      break;
+  }
+    case '/':
+      $('#home').addClass('active');
+      break;
+    case '/upload':
+      $('#upload').addClass('active');
+      break;
+    case '/search':
+      $('#search-footer').addClass('active');
+      break;
+    case '/settings':
+      $('#settings').addClass('active');
+
+});
